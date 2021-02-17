dc141ae458e64637fb754f9740f36e3d738f714b
sidonie <shelliednnguyen@gmail.com>
2021-02-16 16:32:18 -0800

    player object methods and properties

--- __tests__/Player.test.js
+++ __tests__/Player.test.js
@@ -11,3 +11,22 @@ test( "Creates a player object", () => {
    expect( player.agility ).toEqual( expect.any( Number ));
    expect( player.inventory).toEqual( expect.arrayContaining([ expect.any( Object )]));
 });
+
+test( "Gets player's stats as an object", () => {
+   const player = new Player( "Dave" );
+
+   expect( player.getStats()).toHaveProperty( "potions" );
+   expect( player.getStats()).toHaveProperty( "health" );
+   expect( player.getStats()).toHaveProperty( "strength" );
+   expect( player.getStats()).toHaveProperty( "agility" );
+});
+
+test( "Gets Inventory from players or returns false", () => {
+   const player = new Player( "Dave" );
+   
+   expect( player.getInventory()).toEqual( expect.any( Array ));
+
+   player.inventory = [];
+
+   expect( player.getInventory()).toEqual( false );
+});
